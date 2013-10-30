dc109fee0a9bcc9b2f99de0b23f4b72cffc50600
Nikita Kartashov <snailandmail@gmail.com>
2013-10-30 14:49:55 +0400

    Added InsertRows command and types for it, also added new user-specific file to .gitignore.

---
+++ src/dbCommands/TableRow.java
@@ -0,0 +1,20 @@
+package dbCommands;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nikita_kartashov
+ * Date: 23/10/2013
+ * Time: 17:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class TableRow
+{
+    public TableRow(ArrayList<String> arguments)
+    {
+        _arguments = arguments;
+    }
+
+    private ArrayList<String> _arguments;
+}
---
+++ src/memoryManager/DiskPage.java
@@ -0,0 +1,12 @@
+package memoryManager;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nikita_kartashov
+ * Date: 23/10/2013
+ * Time: 17:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class DiskPage {
+    public static final int MAX_PAGE_SIZE = 4096; // 4K
+}
